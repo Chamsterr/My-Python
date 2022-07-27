69f452f4dd1829169a59a69d06df36d6e2a0c079
Chamster <89588942+Chamsterr@users.noreply.github.com>
2022-07-27 23:55:28 +0300

    Update main.py

--- Chess.com API/main.py
+++ Chess.com API/main.py
@@ -1,22 +1,68 @@
-from chessdotcom import get_leaderboards
+from chessdotcom import get_leaderboards, get_player_stats
+from datetime import datetime
+
 import pprint
+import os
+
 
 printer = pprint.PrettyPrinter()
 
 
 def print_leaderboards():
-
     data = get_leaderboards().json
-    # printer.pprint(data)
-
     categories = data['leaderboards']
-    # printer.pprint(categories)
 
     for category in categories:
         print('Category: ', category)
         for idx, entry in enumerate(categories[category]):
-            print(entry)
+            print(f"Rank: {idx + 1}"
+                  f"| Username: {entry['username']} | Rating {entry['score']}")
+
+
+def category_stats(category, username):
+    os.system("cls")
+    tmp = get_player_stats(username).json['stats'][category]
+
+    print(f'{category.replace("_", " ").capitalize()} ')
+    print(f'   Current stats: \n' 
+          f'      Rating: {tmp["last"]["rating"]} \n'
+          f'      Last update: {datetime.fromtimestamp(tmp["last"]["date"])}')
+
+    print(f'   Best game: \n' 
+          f'      Rating: {tmp["best"]["rating"]} \n'
+          f'      Date: {datetime.fromtimestamp(tmp["best"]["date"])}\n'
+          f'      Game: {tmp["best"]["game"]}')
+
+    print(f'   Record: \n'
+          f'      Draw: {tmp["record"]["draw"]} \n'
+          f'      Loss: {tmp["record"]["loss"]} \n'
+          f'      Win: {tmp["record"]["win"]} \n ')
+
+    # tmp = data['stats']['chess_blitz']
+
+
+def get_player_stats_by_category(username):
+    var = input('Choose category (number) \n'
+                '   1.Chess blitz \n'
+                '   2.Chess bullet \n' 
+                '   3.Chess rapid \n'
+                '   4.Lessons \n'
+                '   5.Puzzle rush \n'
+                '   6.Tactics\n')
+    match var:
+        case '1':
+            category_stats('chess_blitz', username)
+        case '2':
+            category_stats('chess_bullet', username)
+        case '3':
+            category_stats('chess_rapid', username)
+        case '4':
+            category_stats('lessons', username)
+        case '5':
+            category_stats('puzzle_rush', username)
+        case '6':
+            category_stats('tactics', username)                
 
 
 
-print_leaderboards()
+get_player_stats_by_category('kazikapa')
