dc79e9e6b32c19832a20fc9f514868e7682eb51f
Chamster <89588942+Chamsterr@users.noreply.github.com>
2022-07-29 23:52:28 +0300

    update get_inventory

--- Steam API/main.py
+++ Steam API/main.py
@@ -1,12 +1,19 @@
+from time import sleep
 import requests
 import pprint
 
 printer = pprint.PrettyPrinter()
 
+
 def get_inventory(steamID):
     url = f'https://steamcommunity.com/profiles/{steamID}/inventory/json/730/2'
-    descriptions = requests.get(url).json()['rgDescriptions']
-    inventory = requests.get(url).json()["rgInventory"]
+    data = requests.get(url)
+    print(data.status_code)
+
+    data = data.json()
+    
+    descriptions = data['rgDescriptions']
+    inventory = data["rgInventory"]
 
     print(f'Counter-Strike: Global Offensive:', len(inventory))
 
