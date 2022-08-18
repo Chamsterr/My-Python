166a5a13cd6a2bc2aded4653815d745af79b28c6
Chamster <89588942+Chamsterr@users.noreply.github.com>
2022-08-18 23:53:15 +0300

    Chapter 3

---
+++ One-liners/Chapter3/3.11.py
@@ -0,0 +1,11 @@
+## Зависимости
+import numpy as np
+## Данные: годовые зарплаты в тысячах долларов (за 2025, 2026 и 2027 гг.)
+
+dataScientist = [130, 132, 137]
+productManager = [127, 140, 145]
+designer = [118, 118, 127]
+softwareEngineer = [129, 131, 137]
+
+empoyees = np.array([dataScientist, productManager, designer, softwareEngineer])
+
---
+++ One-liners/Chapter3/3.2.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+a = np.array([[1, 0, 0],
+              [1, 1, 1],
+              [2, 0, 0]])
+
+b = np.array([[1, 1, 1],
+              [1, 1, 2],
+              [1, 1, 2]])
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+print(np.max(a))
+print(np.min(a))
+print(np.average(a))
