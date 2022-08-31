97f57cb7023385a99b8c966eb9ece6bfcff400d2
Chamster <89588942+Chamsterr@users.noreply.github.com>
2022-08-31 23:56:41 +0300

    Chapter 6

---
+++ One-liners/Chapter6/6.2.py
@@ -0,0 +1,7 @@
+is_palindrome = lambda x1: x1[::-1] == x1
+
+print(is_palindrome('101'))
+
+print(is_palindrome("anna"))
+print(is_palindrome("kdljfasjf"))
+print(is_palindrome("rats live on no evil star"))
